795196d43cf2291cb390bf533cad92f0c490943b
alex <alkatrazzz1993@yandex.ru>
2018-08-29 23:46:25 +0300

    love

---
+++ index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+      	<meta property="og:image" content="LenaLeshaLove.jpg">
+        <link rel="shortcut icon" href="LenaLeshaLove.jpg">
+        <title>Лена & Леша & Любовь</title>
+        <link href="/template/css/fm.revealator.jquery.css" type="text/css" rel="stylesheet">
+<link href="/template/css/fm.revealator.jquery.min.css" type="text/css" rel="stylesheet">
+<link href="/template/css/slick.css" type="text/css" rel="stylesheet">
+<link href="/template/css/slick-theme.css" type="text/css" rel="stylesheet">
+<link href="/template/css/bootstrap.min.css" type="text/css" rel="stylesheet">
+<link href="/template/css/style.css" type="text/css" rel="stylesheet">
+<link href="/upload/images/ico/favicon.ico" type="image/x-icon" rel="shortcut icon">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.9/css/all.css" integrity="sha384-5SOiIsAziJl6AWe0HWRKTXlfcSHKmYV4RBF18PPJ173Kzn7jzMyFuTtk8JA7QQG1" crossorigin="anonymous">
+
+<script src="/template/js/jquery-2.2.0.min.js"></script>
+<script src="/template/js/fm.revealator.jquery.js"></script>
+<script src="/template/js/slick.min.js"></script>
+<script src="/template/js/slider.js"></script>
+<script src="/template/js/bootstrap.min.js"></script>
+<script src="/template/js/jquery.socializer.js"></script>
+<script src="/template/js/highpassword.js" type="text/javascript"></script>
+<script src='https://www.google.com/recaptcha/api.js'></script>
+<script src="/template/js/summernote.js"></script>
+<script src="/template/js/lang/summernote-ru-RU.js"></script>
+<script src="/template/js/script.js"></script>
+<script src="/template/js/formatServices.js"></script>
+<script src="/template/js/buttonActive.js"></script>
+<script src="/template/js/passfield.js" type="text/javascript"></script>
+<script src="/template/js/loading.js"></script>
+<script src="/template/js/delete.js"></script>
+<!-- Подключение jQuery плагина Masked Input -->
+<script src="/template/js/jquery.maskedinput.js"></script>
+			
+
+    </head><!--/head-->
+    <body>
+        
+        <!--header -->
+       <div class="revealator-zoomin revealator-once">	
+        <nav id="header" class="navbar navbar-default">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->										
+                <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <div>
+                <a class="navbar-brand" href="/"> </a>
+                </div>
+                </div>
+             
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="container-fluid"> 
+                    <div class="collapse navbar-collapse" class="collapse" id="bs-example-navbar-collapse-1">                 
+                        <ul class="nav navbar-nav">
+                                                            
+                            <!--<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="Menu"> Меню </span> <span class="caret"></span></a>-->				
+                            <!-- Отображение странички для Admin -->
+                                <ul id="menu-main" class="nav navbar-nav">
+                                    <li><a href="/"><span class="MenuMainPage">Лена & Леша & Love </span></a></li>											  
+                                </ul>					             
+                        </ul>
+              
+                    </div><!-- /.navbar-collapse -->
+                </div><!-- /.container-fluid -->
+        		
+            </div><!-- /.container-fluid -->
+        </nav><!-- navbar navbar-default-->  
+</div><!-- revealator-zoomin revealator-once-->	
+	<div class="clearfix"></div>
+
+
+
+         
+        <!-- center-->
+        <!-- Carusel -->
+<div class="center">
+        <div class="revealator-slideup revealator-delay0 revealator-once">
+            <h1>Я люблю тебя! </h1>
+            
+        </div><!-- revealator-->
+        <div class="clearfix"></div>
+        <br>
+
+
+
+        
+       <div id="footer" class="footer">
+    <div class="col-sm-12">
+        <div class="col-sm-4">
+            <div class="left-footer">
+                
+            </div><!-- left-footer-->
+        </div><!-- col-sm-4-->
+        <div class="col-sm-4"> 
+            <div class="center-footer">                  
+             <p>© 3 сентября 2017 г. - 30 августа 2018г. <br> L&L&L - all rights reserved.</p>
+            </div><!-- center-footer-->          
+        </div><!-- col-sm-4-->
+        <div class="col-sm-4">
+            <div class="right-footer">
+               
+            </div><!-- left-footer-->
+        </div><!--col-sm-4 -->
+    </div><!-- col-sm-12-->
+</div><!--footer-->
+<div class="clearfix"></div>
+
+
+    
+    </body><!-- body-->
+</html>
